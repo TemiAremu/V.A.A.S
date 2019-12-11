7edcf6b25e1870bd681f487f2a2dc2ffeae439bb
Dikachi <dikachi@Dikachis-MacBook-Pro.local>
2019-12-11 11:59:45 -0500

    V instead of U

--- lib/chat.dart
+++ lib/chat.dart
@@ -190,7 +190,7 @@ class ChatMessage extends StatelessWidget {
     return <Widget>[
       new Container(
         margin: const EdgeInsets.only(right: 16.0),
-        child: new CircleAvatar(child: new Text('B')),
+        child: new CircleAvatar(child: new Text('V')),
       ),
       new Expanded(
         child: new Column(
